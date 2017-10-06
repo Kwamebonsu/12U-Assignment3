7c8386d72238b2c5d5b0eab4c58514f2c2a190a6
bonsk5852 <bonsk5852@HRH-IC0027333.wrdsb.ca>
2017-10-06 10:14:59 -0400

    A3Q1 done

--- src/A3Q5.java
+++ src/A3Q5.java
@@ -9,23 +9,27 @@
  */
 public class A3Q5 {
 
-    public void swap(int[] array, int p1, int p2) {
-        int temp = array[p1];
+    public void swap(String[] array, String p1, String p2) {
+        String temp = array[p1];
         array[p1] = array[p2];
         array[p2] = temp;
     }
 
-    public void insertion(int[] array) {
+    public void insertion(String[] array) {
         // Start going through the array
         for (int i = 0; i < array.length - 1; i++) {
             // Store position
             int position = i;
             // Check values beside each other
-            while (position >= 0 && array[position] > array[position + 1]) {
-                // If out of place, swap it downwards until correct position is reached
-                swap(array, position, position + 1);
-                position--;
+
+            if (array[i].compareTo(array[i + 1]) > 0) {
+                swap(array, array[i], array[i + 1]);
             }
+//            while (position >= 0 && array[position] > array[position + 1]) {
+//                // If out of place, swap it downwards until correct position is reached
+//                swap(array, position, position + 1);
+//                position--;
+//            }
         }
 
     }
@@ -34,18 +38,20 @@ public class A3Q5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int numbers[] = new int[10];
-        numbers[0] = 0;
-        numbers[1] = 11;
-        numbers[2] = 8;
-        numbers[3] = 4;
-        numbers[4] = 7;
-        numbers[5] = 3;
-        numbers[6] = 6;
-        numbers[7] = 9;
-        numbers[8] = 1;
-        numbers[9] = 11;
+        // Create an array
+        String numbers[] = new String[10];
+        numbers[0] = "Hello";
+        numbers[1] = "My";
+        numbers[2] = "name";
+        numbers[3] = "is";
+        numbers[4] = "Kwame";
+        numbers[5] = "bOnsu";
+        numbers[6] = "Word";
+        numbers[7] = "letter";
+        numbers[8] = "array";
+        numbers[9] = "Java";
 
+        // Test the insertion method
         A3Q5 test = new A3Q5();
         test.insertion(numbers);
         for (int i = 0; i < numbers.length; i++) {
