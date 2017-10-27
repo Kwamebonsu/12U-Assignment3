7d70d9a12c0688c20ea70cd2c4f2537c38aecc2d
bonsk5852 <bonsk5852@HRH-IC0027333.wrdsb.ca>
2017-10-27 10:07:14 -0400

    Finish A3Q4 and Written Questions

--- src/A3Q4.java
+++ src/A3Q4.java
@@ -11,18 +11,27 @@ public class A3Q4 {
     // Create a method
 
     public void countingSort(int[] array) {
-        // Create a new array
+        // Create a new array and fill it with 0s
         int[] counter = new int[101];
+        for (int i = 0; i < counter.length; i++) {
+            counter[i] = 0;
+        }
         // Go through the array
         for (int i = 0; i < counter.length; i++) {
+
+            // Compare the number at position p to i.
             for (int p = 0; p < array.length; p++) {
-                if(array[p] == i){
-                    counter[i] = counter[i]++;
+                if (i == array[p]) {
+                    counter[i]++;
                 }
             }
         }
         // Count the number of array[1] in the array
-        // 
+
+        // Output the sorted array
+        for (int i = 0; i < counter.length; i++) {
+            System.out.println("Kwame " + array[i]);
+        }
     }
 
     /**
@@ -31,7 +40,7 @@ public class A3Q4 {
     public static void main(String[] args) {
         // Test the method
         A3Q4 test = new A3Q4();
-        
+
         // Create an array
         int[] array = new int[100];
         // Fill the array with random numbers
